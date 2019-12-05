8496add70fba69a72478a26cb5802b78dcaf2cf3
Alexandr K <Dgordg85@gmail.com>
2019-12-05 17:31:18 +0300

    Первая часть

---
+++ src/test/java/ru/geekbrains/todolist/GeekTodolistApplicationTests.java
@@ -0,0 +1,17 @@
+package ru.geekbrains.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	private static void	todo(int arr){
+
+	}
+
+}
