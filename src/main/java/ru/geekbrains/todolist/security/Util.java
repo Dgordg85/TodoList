f49f468b0829b39bab0e16d3ced0646e016c2af1
Alexandr K <Dgordg85@gmail.com>
2019-12-08 04:01:27 +0300

    Ошибки

---
+++ src/main/java/ru/geekbrains/todolist/security/Util.java
@@ -0,0 +1,17 @@
+package ru.geekbrains.todolist.security;
+
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public final class Util {
+    public static Optional<String> getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            return Optional.of(authentication.getName());
+        }
+        return Optional.empty();
+    }
+}
